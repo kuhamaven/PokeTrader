6422ea254218d03a763f10c1b57f4f8c10700e1b
kuhamaven <khalil.stessens@ing.austral.edu.ar>
2020-06-30 22:23:13 -0300

    Finishing trade logic WIP

--- src/app/exploretrades/exploretrades.module.ts
+++ src/app/exploretrades/exploretrades.module.ts
@@ -4,12 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ExploretradesRoutingModule } from './exploretrades-routing.module';
 import { ExploretradesComponent } from './exploretrades.component';
 import { TradecomponentComponent } from './tradecomponent/tradecomponent.component';
-import { BidcreatorcomponentComponent } from './bidcreatorcomponent/bidcreatorcomponent.component';
 import { BiddercardComponent } from './biddercard/biddercard.component';
 
 
 @NgModule({
-  declarations: [ExploretradesComponent, TradecomponentComponent, BidcreatorcomponentComponent, BiddercardComponent],
+  declarations: [ExploretradesComponent, TradecomponentComponent, BiddercardComponent],
   imports: [
     CommonModule,
     ExploretradesRoutingModule
